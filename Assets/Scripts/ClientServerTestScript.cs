775029f0fc003fc0477f36688a19dd0201194441
17evanml <evanmliu@gmail.com>
2020-03-26 17:46:30 -0400

    Initial Multiplayer Architecture

---
+++ Assets/Scripts/ClientServerTestScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientServerTestScript : MonoBehaviour
+{
+
+    public Server server;
+    // Start is called before the first frame update
+    void Start()
+    {
+        server = new Server();
+        server.Start(10, 6942);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    public GameObject startMenu;
+    public InputField usernameField;
+    public InputField ipField;
+    public InputField portField;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this);
+        }
+    }
+
+    public void ConnectToServer()
+    {
+        startMenu.SetActive(false);
+        usernameField.interactable = false;
+        Client.instance.ip = ipField.text;
+        Client.instance.port = int.Parse(portField.text);
+        Client.instance.ConnectToServer();
+    }
+}
