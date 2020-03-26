d9a323a75dd7704ec58d30aee0b2f74ffdd70072
17evanml <evanmliu@gmail.com>
2020-03-26 17:46:30 -0400

    Initial Multiplayer Architecture

---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System;
+
+public class Server
+{
+    public static int MaxPlayers { get; private set; }
+    public static int Port { get; private set; }
+
+    public static Dictionary<int, ServerClient> clients = new Dictionary<int, ServerClient>();
+
+    private static TcpListener tcpListener;
+    public void Start(int maxPlayers, int port)
+    {
+        MaxPlayers = maxPlayers;
+        Port = port;
+
+        Debug.Log("Starting Server");
+        InitalizeServer();
+
+        tcpListener = new TcpListener(IPAddress.Any, Port);
+        tcpListener.Start();
+        tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+
+        Debug.Log($"Server started on {Port}.");
+    }
+
+    private static void TCPConnectCallback(IAsyncResult _result)
+    {
+        TcpClient _client = tcpListener.EndAcceptTcpClient(_result);
+        tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+        Debug.Log($" Incoming connection from: {_client.Client.RemoteEndPoint}");
+
+
+
+        for (int i = 1; i <= MaxPlayers; i++)
+        {
+            if(clients[i].tcp.socket == null)
+            {
+                clients[i].tcp.Connect(_client);
+                return;
+            }
+        }
+        Debug.Log($"{_client.Client.RemoteEndPoint} failed to connect: Server full");
+    }
+
+    private static void InitalizeServer()
+    {
+        for(int i = 1; i <= MaxPlayers; i++)
+        {
+            clients.Add(i, new ServerClient(i));
+
+        }
+
+    }
+}
