6189fb8f05196d308b21c790954e2aacd9c01869
17evanml <evanmliu@gmail.com>
2020-03-27 22:14:16 -0400

    Overhauled entire multiplayer system.

---
+++ Assets/Scripts/Server Scripts/NetworkManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour
+{
+    public static NetworkManager instance;
+
+    public GameObject playerPrefab;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            InstantiatePlayer();
+        }
+    }
+
+
+    private void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 30;
+
+        Server.Start(50, 6942);
+    }
+
+    public Player InstantiatePlayer()
+    {
+        return Instantiate(playerPrefab, Vector3.zero, Quaternion.identity).GetComponent<Player>();
+    }
+}
