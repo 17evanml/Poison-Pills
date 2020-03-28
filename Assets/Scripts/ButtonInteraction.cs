bb8cf0743a5c23e241c4494ccbfda71a2b492b43
Arjunj99 <asj397@nyu.edu>
2020-03-28 02:18:30 -0400

    Added UI Scripts

---
+++ Assets/Scripts/ButtonInteraction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ButtonInteraction : MonoBehaviour {
+    public bool buttonHover = false;
+    public Vector3 originalScale;
+
+    // Start is called before the first frame update
+    void Start() {
+        originalScale = this.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // if (hover) { OnHover(); Debug.Log("ON HOVER"); }
+        // else { OffHover(); }
+    }
+
+    public abstract void OnHover();
+
+    public abstract void OffHover();
+
+    void OnMouseEnter() { buttonHover = true; OnHover(); }
+
+    void OnMouseExit() { buttonHover = false; OffHover(); }
+
+    public abstract void OnClick();
+
+    void OnMouseDown() { OnClick(); }
+}
--- Assets/Scripts/TwoOptionsHover.cs
+++ Assets/Scripts/TwoOptionsHover.cs
@@ -3,19 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TwoOptionsHover : CupInteraction {
-
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public override void OnHover() {
+        button1.transform.localScale = Vector3.one; /*Vector3.Lerp(button1.transform.localScale, originalScale, Time.deltaTime);*/
+        button2.transform.localScale = Vector3.one; /*Vector3.Lerp(button2.transform.localScale, originalScale, Time.deltaTime);*/
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public override void OffHover() {
+        button1.transform.localScale = Vector3.zero; /*Vector3.Lerp(button1.transform.localScale, new Vector3(0.1f, 0.1f, 0.1f), Time.deltaTime);*/
+        button2.transform.localScale = Vector3.zero; /*Vector3.Lerp(button2.transform.localScale, new Vector3(0.1f, 0.1f, 0.1f), Time.deltaTime);*/
     }
 }
