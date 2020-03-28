93665c2739054709b2868791b81e7d85d916791e
pollivie <pollivie@usc.edu>
2020-03-28 12:27:49 -0700

    Added pseudocode to cup class

--- Assets/Prefabs/PFB_Cup/CupInfo.cs
+++ Assets/Prefabs/PFB_Cup/CupInfo.cs
@@ -7,6 +7,10 @@ public class CupInfo : MonoBehaviour
     private int id;
     private string playerName;
     private Color32 color;
+    private Stack<Pill> pillStack = new Stack<Pill>();
+
+    public Vector3 originalScale;
+    public bool hover = false;
 
     CupInfo(int id, string name, Color32 color)
     {
@@ -24,14 +28,39 @@ public class CupInfo : MonoBehaviour
 
     public void OnHover()
     {
-
+        //Outline cup?
+        //Set cup bool onHover to be true
     }
 
     public void OffHover()
     {
-
+        //Set cup bool onHover to be false
     }
 
+    public void OnClick(CursorController player)
+    {
+        //Call the button display script
+        //All three buttons are not instantiated, they are saved as a reference in the scene
+        //Moves buttons to right place (based on offset)
+        //SETS background button to be OFFCLICK for this CUPINFO class
+        //SET RAND LEFT OR RIGHT BUTTON TO BE AddPill(player.poisonPill)...
+        //Enables UI
+    }
+
+    public void OffClick()
+    {
+        //Call the button hide script
+        //DISABLES UI
+    }
+
+    public void AddPill(Pill pill)
+    {
+        pillStack.Push(pill);
+        OffClick();
+        CursorGameManager.Instance.NextTurn();
+    }
+
+    //OPERATOR OVERLOADING
     public override bool Equals(object obj)
     {
         return this.Equals(obj as CupInfo);
