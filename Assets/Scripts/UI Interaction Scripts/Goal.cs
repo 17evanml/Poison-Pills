aff0f14eaa3dae037b568cc20b291113ee046a1d
Arjunj99 <asj397@nyu.edu>
2020-03-30 00:39:03 -0400

    Added Goal Display and Goal Scripts

---
+++ Assets/Scripts/UI Interaction Scripts/Goal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary> Representation of a Player Goal. </summary>
+public class Goal {
+    public int id; // Target Player ID
+    public enum goalOptions { die, live } // What goals you can be given
+    public goalOptions goal; // Players' goal
+
+    /// <summary> Representation of a Player Goal. </summary>
+    /// <param name="id"> Target Player ID. </param>
+    /// <param name="goal"> Current Player Goal. </param>
+    public Goal (int id, goalOptions goal) {
+        this.id = id;
+        this.goal = goal;
+    }
+}
