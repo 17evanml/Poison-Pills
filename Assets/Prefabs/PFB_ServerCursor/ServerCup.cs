4306d5d55816a9b1c915d94f82a910c497586cc9
17evanml <evanmliu@gmail.com>
2020-03-30 02:24:16 -0400

    Added netcode for cups and pills

---
+++ Assets/Prefabs/PFB_ServerCursor/ServerCup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerCup : MonoBehaviour
+{
+    public int id;
+    public int username;
+    public Stack<Pill> pillStack;
+
+    public void AddPill(Pill pill)
+    {
+        pillStack.Push(pill);
+
+    }
+}
