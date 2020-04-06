ebea71563b740abcc43899359ee20bee756e0752
pollivie <pollivie@usc.edu>
2020-04-06 12:27:19 -0700

    Changed cursor lerping and image

--- Assets/Scripts/CursorManager.cs
+++ Assets/Scripts/CursorManager.cs
@@ -10,8 +10,8 @@ public class CursorManager : MonoBehaviour
 
     public Vector2 mousePosition; //SERVER NEEDS TO KNOW
     public Vector2 cursorSizes = new Vector2(64, 128);
-    private Vector2 lerpPosition = new Vector2(0, 0);
-    private Vector2 tempPos = new Vector2();
+    public Vector2 lerpPosition = new Vector2(0, 0);
+    private Vector2 tempPos = new Vector2(0, 0);
     public int cursorSize = 64;
     public Texture2D cursorTexture;
 
@@ -31,8 +31,8 @@ public class CursorManager : MonoBehaviour
         else
         {
             GUI.color = color;
-            tempPos = mousePosition;
-            lerpPosition = (tempPos + lerpPosition) / 2;
+            tempPos = (mousePosition + tempPos)/2;
+            lerpPosition = tempPos;
             lerpPosition.x *= Screen.width / 2;
             lerpPosition.y *= Screen.height / 2;
             lerpPosition.x += Screen.width / 2;
