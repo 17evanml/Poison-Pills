a6f27192f3c1c441c9601c0e2212a312055f07f2
Adriene Tien <adrienetien@Adrienes-MacBook-Air.local>
2020-04-09 13:23:51 -0400

    Letting arjun see scripts

--- Assets/Scripts/UIBase.cs
+++ Assets/Scripts/UIBase.cs
@@ -3,40 +3,27 @@ using System.Collections.Generic;
 using UnityEngine.EventSystems;
 using UnityEngine;
 
-public class UIBase : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+public abstract class UIBase : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
     [Header ("General Settings")]
     public float delay = 0.5f;
     public float speed = 0.5f;
-    private bool beenClicked = false;
+    [HideInInspector] public bool beenClicked = false;
 
     [Header ("On Hover Settings")]
     public Vector3 deltaScale = Vector3.zero;
-    private Vector3 onHoverScale = Vector3.one;
+    [HideInInspector] public Vector3 onHoverScale = Vector3.one;
     public LeanTweenType onHover = LeanTweenType.easeInElastic;
 
     [Header ("Off Hover Settings")]
-    private Vector3 offHoverScale = Vector3.one;
+    [HideInInspector] public Vector3 offHoverScale = Vector3.one;
     public LeanTweenType offHover = LeanTweenType.easeOutElastic;
 
     [Header ("On Click Settings")]
     public Vector3 onClickScale = Vector3.zero;
     public Vector3 deltaPosition = Vector3.zero;
-    private Vector3 onClickPosition = Vector3.zero;
+    [HideInInspector] public Vector3 onClickPosition = Vector3.zero;
     public LeanTweenType onClick = LeanTweenType.easeOutElastic;
     public LeanTweenType onClickMove = LeanTweenType.easeOutElastic;
-
-    // Start is called before the first frame update
-    void Start() {
-        offHoverScale = transform.localScale;
-        onHoverScale = offHoverScale + deltaScale;
-        onClickPosition = transform.position + deltaPosition;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void OnPointerEnter(PointerEventData data) {
         if (!beenClicked)
@@ -48,9 +35,5 @@ public class UIBase : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
             LeanTween.scale(gameObject, offHoverScale, speed).setDelay(delay).setEase(offHover);
     }
 
-    public void OnClick() {
-        LeanTween.scale(gameObject, onClickScale, speed).setDelay(delay).setEase(onClick);
-        // LeanTween.move(gameObject, deltaPosition, speed).setDelay(delay).setEase(onClickMove);
-        beenClicked = true;
-    }
+    public abstract void OnClick();
 }
---
+++ Assets/Scripts/UiOnClickChange.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiOnClickChange : UIBase
+{
+
+    void Start()
+    {
+        offHoverScale = transform.localScale;
+        onHoverScale = offHoverScale + deltaScale;
+        onClickPosition = transform.position + deltaPosition;
+    }
+
+    /// <summary>
+    /// For UI that is supposed to change after clicking. Scale to custom set.
+    /// </summary>
+    public override void OnClick()
+    {
+
+    }
+
+}
