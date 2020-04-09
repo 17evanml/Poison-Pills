dc4cf4be50ca47455ea82b9dec3246bcd51af135
Adriene Tien <17tiena@gmail.com>
2020-04-09 13:23:51 -0400

    Letting arjun see scripts

---
+++ Assets/Scripts/UiOnClickDisappear.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiOnClickDisappear : UIBase
+{
+    void Start()
+    {
+        offHoverScale = transform.localScale;
+        onHoverScale = offHoverScale + deltaScale;
+        onClickPosition = transform.position + deltaPosition;
+    }
+
+    /// <summary>
+    /// For UI that is supposed to disappear after clicking. Scale down to 0.
+    /// </summary>
+    public override void OnClick()
+    {
+        
+    }
+
+}
