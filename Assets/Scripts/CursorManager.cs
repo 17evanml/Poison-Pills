f312ac20a8dadac8522e65b08fab6aabdd89f225
pollivie <pollivie@usc.edu>
2020-05-22 18:48:02 -0700

    Change mouse position to world pos

--- Assets/Scripts/CursorManager.cs
+++ Assets/Scripts/CursorManager.cs
@@ -8,36 +8,35 @@ public class CursorManager : MonoBehaviour
     public string username;
     public Color32 color = new Color32(255, 255, 255, 255);
 
-    public Vector2 mousePosition; //SERVER NEEDS TO KNOW
+    public Vector3 mousePosition;
     public Vector2 cursorSizes = new Vector2(64, 128);
     public Vector2 lerpPosition = new Vector2(0, 0);
     private Vector2 tempPos = new Vector2(0, 0);
+    private Camera cam;
     
     public int cursorSize = 64;
     public Texture2D cursorTexture;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
 
     void OnGUI()
     {
         if(Client.instance.myId == id)
         {
             GUI.color = color;
-            tempPos = mousePosition;
-            tempPos.x *= Screen.width / 2;
-            tempPos.y *= Screen.height / 2;
-            tempPos.x += Screen.width / 2;
-            tempPos.y += Screen.height / 2;
+            tempPos = cam.WorldToScreenPoint(mousePosition);
             tempPos += Vector2.one * 16;
             GUI.DrawTexture(new Rect(tempPos.x - (cursorSize / 2), tempPos.y - (cursorSize / 2), cursorSize, cursorSize), cursorTexture);
         }
         else
         {
             GUI.color = color;
-            tempPos = (mousePosition + tempPos)/2;
-            lerpPosition = tempPos;
-            lerpPosition.x *= Screen.width / 2;
-            lerpPosition.y *= Screen.height / 2;
-            lerpPosition.x += Screen.width / 2;
-            lerpPosition.y += Screen.height / 2;
+            tempPos = cam.WorldToScreenPoint(mousePosition);
+            lerpPosition += tempPos;
+            lerpPosition /= 2;
             lerpPosition += Vector2.one * 16;
             GUI.DrawTexture(new Rect(lerpPosition.x - (cursorSize / 2), lerpPosition.y - (cursorSize / 2), cursorSize, cursorSize), cursorTexture);
         }
@@ -76,7 +75,7 @@ public class CursorManager : MonoBehaviour
         cursorSize = (int)cursorSizes.x;
     }
 
-    public void SetMousePosition(Vector2 _mousePosition)
+    public void SetMousePosition(Vector3 _mousePosition)
     {
         mousePosition = _mousePosition;
     }
