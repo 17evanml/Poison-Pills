dbe58469c9dda26c36f2f4a93240d71d8645718e
Adriene Tien <17tiena@gmail.com>
2020-05-24 18:27:03 -0400

    Updated Audio manager and constants based on suggestions.

--- Assets/Scripts/Server Scripts/Constants.cs
+++ Assets/Scripts/Server Scripts/Constants.cs
@@ -8,7 +8,7 @@ public static class Constants
     public const float MS_PER_TICK = 1000f / TICKS_PER_SEC; // How many milliseconds per tick
 
     // Music 
-    public const string smMusic = "SM_BassWalker";
-    public const string lmMusic = "LM_CovertAffairs";
-    public const string inGameMusic = "InGame_Noire";
+    public const string SM_MUSIC = "SM_BassWalker";
+    public const string LM_MUSIC = "LM_CovertAffairs";
+    public const string INGAME_MUSIC = "InGame_Noire";
 }
